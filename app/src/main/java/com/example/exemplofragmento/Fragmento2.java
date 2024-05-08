35c3b5076922b3cfeb6e0c42ad7a5b534c5f947e
Macedolaz <133664855+Macedolaz@users.noreply.github.com>
2024-05-07 23:23:20 -0300

    Update Fragmento2.java

--- app/src/main/java/com/example/exemplofragmento/Fragmento2.java
+++ app/src/main/java/com/example/exemplofragmento/Fragmento2.java
@@ -11,6 +11,53 @@ import androidx.fragment.app.FragmentContainer;
 
 public class Fragmento2 extends Fragment {
     static  View frgto2;
+    private Button Botao4;
+    private Button Botao5;
+    ViewGroup c;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        frgto2 = inflater.inflate(R.layout.fragmento2, container, false);
+
+        Botao4 = (Button) frgto2.findViewById(R.id.buttonA);
+        Botao4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView txt = (TextView) Fragmento2.frgto2.findViewById(R.id.texto_frg2);
+                txt.setText("");
+                Date currentDate = new Date();
+
+                String timeZoneID = "America/Sao_Paulo";
+                TimeZone timeZone = TimeZone.getTimeZone(timeZoneID);
+
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                sdf.setTimeZone(timeZone);
+                String formattedDate = sdf.format(currentDate);
+
+                txt.setTextColor(Color.BLACK);
+                txt.setText(formattedDate);
+
+
+            }
+        });
+
+        Botao5 = (Button) frgto2.findViewById(R.id.buttonB);
+        Botao5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment fragmentoData = new FragmentoOtherDatePicker();
+                fragmentoData.show(getParentFragmentManager(), "datePicker");
+
+            }
+        });
+
+    }
+}
     ViewGroup c;
     @Override
     public void onCreate(Bundle savedInstanceState) {
